20056fbdb4f21b79e923e40becbf5788c46f0094
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2025-02-20 14:31:38 +0530

    Improved UI

--- Modules/Authentication_&_Authorization/View/Logout/Logout.php
+++ Modules/Authentication_&_Authorization/View/Logout/Logout.php
@@ -6,6 +6,6 @@ session_start();
 session_unset();
 session_destroy();
 
-header('Location: ../Login/login.php');
+header('Location: ../../../Authentication_&_Authorization/View/Login/login.php');
 
 ?>
