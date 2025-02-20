0081b8d6333bc6a183a2cd5f0959696abf08b142
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2025-02-20 17:49:28 +0530

    Updated Mailer

--- Modules/Mailer/Mailer.php
+++ Modules/Mailer/Mailer.php
@@ -15,11 +15,11 @@
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'aryan.langhanoja119561@marwadiuniversity.ac.in';
-        $mail->Password = 'cclq ktox eqlz ljdx'; // Warning: Do not expose sensitive credentials in code
+        $mail->Username = 'langhanojaaryan93@gmail.com';
+        $mail->Password = 'wpdr wyzb cmri mkpb'; // Warning: Do not expose sensitive credentials in code
         $mail->SMTPSecure = 'ssl';
         $mail->Port = 465;
-        $mail->setFrom('aryan.langhanoja119561@marwadiuniversity.ac.in');
+        $mail->setFrom('langhanojaaryan93@gmail.com');
         $mail->addAddress($email);
         $mail->isHTML(true);
         $mail->Subject = $subject;
